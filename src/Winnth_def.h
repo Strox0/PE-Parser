1911223f65a666bc5d890ed49ee26ec41a6248b5
Strox0 <molnartamas332@gmail.com>
2024-04-27 18:49:47 +0200

    Struct Defninitions added for parsing

---
+++ src/Winnth_def.h
@@ -0,0 +1,172 @@
+#pragma once
+
+#define IMAGE_NUMBEROF_DIRECTORY_ENTRIES 16
+
+#define IMAGE_NT_OPTIONAL_HDR32_MAGIC      0x10b
+#define IMAGE_NT_OPTIONAL_HDR64_MAGIC      0x20b
+#define IMAGE_ROM_OPTIONAL_HDR_MAGIC       0x107
+
+#define IMAGE_DOS_SIGNATURE                 0x5A4D      // MZ
+#define IMAGE_NT_SIGNATURE                  0x00004550  // PE00
+
+#define IMAGE_DIRECTORY_ENTRY_EXPORT          0   // Export Directory
+#define IMAGE_DIRECTORY_ENTRY_IMPORT          1   // Import Directory
+#define IMAGE_DIRECTORY_ENTRY_RESOURCE        2   // Resource Directory
+#define IMAGE_DIRECTORY_ENTRY_EXCEPTION       3   // Exception Directory
+#define IMAGE_DIRECTORY_ENTRY_SECURITY        4   // Security Directory
+#define IMAGE_DIRECTORY_ENTRY_BASERELOC       5   // Base Relocation Table
+#define IMAGE_DIRECTORY_ENTRY_DEBUG           6   // Debug Directory
+#define IMAGE_DIRECTORY_ENTRY_ARCHITECTURE    7   // Architecture Specific Data
+#define IMAGE_DIRECTORY_ENTRY_GLOBALPTR       8   // RVA of GP
+#define IMAGE_DIRECTORY_ENTRY_TLS             9   // TLS Directory
+#define IMAGE_DIRECTORY_ENTRY_LOAD_CONFIG    10   // Load Configuration Directory
+#define IMAGE_DIRECTORY_ENTRY_BOUND_IMPORT   11   // Bound Import Directory in headers
+#define IMAGE_DIRECTORY_ENTRY_IAT            12   // Import Address Table
+#define IMAGE_DIRECTORY_ENTRY_DELAY_IMPORT   13   // Delay Load Import Descriptors
+#define IMAGE_DIRECTORY_ENTRY_COM_DESCRIPTOR 14   // COM Runtime descriptor
+
+#define IMAGE_SCN_CNT_INITIALIZED_DATA		0x00000040
+#define IMAGE_SCN_CNT_UNINITIALIZED_DATA	0x00000080
+#define IMAGE_SCN_MEM_READ					0x40000000
+
+#define IMAGE_SIZEOF_SHORT_NAME 8
+
+#define ILT_FLAG_ORDINAL 0x80000000
+
+typedef unsigned long DWORD;
+typedef unsigned short WORD;
+typedef unsigned long long ULONGLONG;
+typedef long long LONGLONG;
+typedef unsigned char BYTE;
+typedef long LONG;
+typedef unsigned long long DWORD64;
+typedef DWORD64 QWORD;
+
+typedef struct _IMAGE_DATA_DIRECTORY {
+	DWORD   VirtualAddress;
+	DWORD   Size;
+} IMAGE_DATA_DIRECTORY, * PIMAGE_DATA_DIRECTORY;
+
+typedef struct _IMAGE_FILE_HEADER {
+	WORD    Machine;
+	WORD    NumberOfSections;
+	DWORD   TimeDateStamp;
+	DWORD   PointerToSymbolTable;
+	DWORD   NumberOfSymbols;
+	WORD    SizeOfOptionalHeader;
+	WORD    Characteristics;
+} IMAGE_FILE_HEADER, * PIMAGE_FILE_HEADER;
+
+typedef struct _IMAGE_OPTIONAL_HEADER64 {
+	WORD        Magic;
+	BYTE        MajorLinkerVersion;
+	BYTE        MinorLinkerVersion;
+	DWORD       SizeOfCode;
+	DWORD       SizeOfInitializedData;
+	DWORD       SizeOfUninitializedData;
+	DWORD       AddressOfEntryPoint;
+	DWORD       BaseOfCode;
+	ULONGLONG   ImageBase;
+	DWORD       SectionAlignment;
+	DWORD       FileAlignment;
+	WORD        MajorOperatingSystemVersion;
+	WORD        MinorOperatingSystemVersion;
+	WORD        MajorImageVersion;
+	WORD        MinorImageVersion;
+	WORD        MajorSubsystemVersion;
+	WORD        MinorSubsystemVersion;
+	DWORD       Win32VersionValue;
+	DWORD       SizeOfImage;
+	DWORD       SizeOfHeaders;
+	DWORD       CheckSum;
+	WORD        Subsystem;
+	WORD        DllCharacteristics;
+	ULONGLONG   SizeOfStackReserve;
+	ULONGLONG   SizeOfStackCommit;
+	ULONGLONG   SizeOfHeapReserve;
+	ULONGLONG   SizeOfHeapCommit;
+	DWORD       LoaderFlags;
+	DWORD       NumberOfRvaAndSizes;
+	IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
+} IMAGE_OPTIONAL_HEADER64, * PIMAGE_OPTIONAL_HEADER64;
+
+typedef struct _IMAGE_NT_HEADERS64 {
+	DWORD Signature;
+	IMAGE_FILE_HEADER FileHeader;
+	IMAGE_OPTIONAL_HEADER64 OptionalHeader;
+} IMAGE_NT_HEADERS64, * PIMAGE_NT_HEADERS64;
+
+#ifdef _WIN64
+typedef IMAGE_NT_HEADERS64                  IMAGE_NT_HEADERS;
+typedef PIMAGE_NT_HEADERS64                 PIMAGE_NT_HEADERS;
+#else
+typedef IMAGE_NT_HEADERS32                  IMAGE_NT_HEADERS;
+typedef PIMAGE_NT_HEADERS32                 PIMAGE_NT_HEADERS;
+#endif
+
+typedef struct _IMAGE_SECTION_HEADER {
+	BYTE    Name[IMAGE_SIZEOF_SHORT_NAME];
+	union {
+		DWORD   PhysicalAddress;
+		DWORD   VirtualSize;
+	} Misc;
+	DWORD   VirtualAddress;
+	DWORD   SizeOfRawData;
+	DWORD   PointerToRawData;
+	DWORD   PointerToRelocations;
+	DWORD   PointerToLinenumbers;
+	WORD    NumberOfRelocations;
+	WORD    NumberOfLinenumbers;
+	DWORD   Characteristics;
+} IMAGE_SECTION_HEADER, * PIMAGE_SECTION_HEADER;
+
+typedef struct _IMAGE_DOS_HEADER {      // DOS .EXE header
+	WORD   e_magic;                     // Magic number
+	WORD   e_cblp;                      // Bytes on last page of file
+	WORD   e_cp;                        // Pages in file
+	WORD   e_crlc;                      // Relocations
+	WORD   e_cparhdr;                   // Size of header in paragraphs
+	WORD   e_minalloc;                  // Minimum extra paragraphs needed
+	WORD   e_maxalloc;                  // Maximum extra paragraphs needed
+	WORD   e_ss;                        // Initial (relative) SS value
+	WORD   e_sp;                        // Initial SP value
+	WORD   e_csum;                      // Checksum
+	WORD   e_ip;                        // Initial IP value
+	WORD   e_cs;                        // Initial (relative) CS value
+	WORD   e_lfarlc;                    // File address of relocation table
+	WORD   e_ovno;                      // Overlay number
+	WORD   e_res[4];                    // Reserved words
+	WORD   e_oemid;                     // OEM identifier (for e_oeminfo)
+	WORD   e_oeminfo;                   // OEM information; e_oemid specific
+	WORD   e_res2[10];                  // Reserved words
+	LONG   e_lfanew;                    // File address of new exe header
+} IMAGE_DOS_HEADER, * PIMAGE_DOS_HEADER;
+
+typedef struct _IMAGE_IMPORT_DESCRIPTOR {
+	union {
+		DWORD   Characteristics;
+		DWORD   OriginalFirstThunk;
+	} DUMMYUNIONNAME;
+	DWORD   TimeDateStamp;
+	DWORD   ForwarderChain;
+	DWORD   Name;
+	DWORD   FirstThunk;
+} IMAGE_IMPORT_DESCRIPTOR;
+typedef IMAGE_IMPORT_DESCRIPTOR*  PIMAGE_IMPORT_DESCRIPTOR;
+
+typedef struct _IMAGE_BASE_RELOCATION {
+	DWORD   VirtualAddress;
+	DWORD   SizeOfBlock;
+} IMAGE_BASE_RELOCATION;
+typedef IMAGE_BASE_RELOCATION* PIMAGE_BASE_RELOCATION;
+
+struct ILT_ENTRY_64
+{
+	union
+	{
+		WORD Ordinal;
+		DWORD HintTable;
+	};
+	DWORD Flag;
+};
+typedef ILT_ENTRY_64* PILT_ENTRY_64;
