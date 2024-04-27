995a31201694a440f3b0d95b26f29e442b9dcfec
Strox0 <molnartamas332@gmail.com>
2024-04-27 12:24:49 +0200

    Inital commit

---
+++ setup.lua
@@ -0,0 +1,39 @@
+outputdir = "%{cfg.buildcfg}-%{cfg.architecture}"
+
+workspace "PE-Parser"
+	architecture "x64"
+	configurations {"Debug","Release"}
+
+    filter "system:windows"
+      buildoptions { "/EHsc", "/Zc:preprocessor", "/Zc:__cplusplus" }
+      systemversion "latest"
+
+    filter "configurations:Debug"
+      defines { "_DEBUG" }
+      runtime "Debug"
+      symbols "On"
+
+    filter "configurations:Release"
+      defines { "NDEBUG" }
+      runtime "Release"
+      optimize "On"
+      symbols "Off"
+
+project "PE-Parser"
+	kind "Console"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/")
+	objdir ("bin-int/" .. outputdir .. "/")
+
+	includedirs {
+		"src/"
+	}
+
+	files {
+		"src/**.cpp",
+		"src/**.h",
+		"src/**.c"
+	}
